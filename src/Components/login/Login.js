32320adef493c0f66e85008710e6259a6488718d
mouayaid <mouayaidchatti@gmail.com>
2024-02-18 03:36:23 +0100

    o

---
+++ src/Components/login/Login.js
@@ -0,0 +1,45 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import imgLog from "../Images/login_img.png";
+import Table from "react-bootstrap/Table";
+
+function Login() {
+  return (
+    <Container className="justify-content-center"> {/* Apply justify-content-center class */}
+      <Table>
+        <tr>
+          <td>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </td>
+          <td>
+            <img src={imgLog} style={{ height: "300px" }} />
+          </td>
+        </tr>
+      </Table>
+    </Container>
+  );
+}
+
+export default Login;
